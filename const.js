fb4c78708d517470c51527d08573fc028c557c6e
Daniil <daniil42rus@mail.ru>
2022-03-16 16:31:41 +0700

    ver1

---
+++ const.js
@@ -0,0 +1,7 @@
+const command = `
+/start - Перезапустить бота
+/new_application - Подать заявку 
+/help - Помощь
+`
+
+module.exports.command = command;
--- index.js
+++ index.js
@@ -1,32 +1,636 @@
-const { Telegraf } = require('telegraf')
+const command = require('nodemon/lib/config/command');
+const TelegramBot = require('node-telegram-bot-api');
+const { Telegraf, Markup } = require('telegraf')
 const { Keyboard } = require('telegram-keyboard');
+require('dotenv').config();
+const text = require('./const');
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const botMessage = new TelegramBot(process.env.BOT_TOKEN);
 
-
-const token = '5289196198:AAFqEmyAtwIUH2lahhzJKmRR_dXor2VVzZA';
-
-
-const bot = new Telegraf(token)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-
-
+bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name} ${text.command}`))
+bot.help((ctx) => ctx.reply(text.command))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.hears('Привет', (ctx) => ctx.reply(`Привет ${text.command}`))
+
+
+
+
+
+
+
+
+
+
+bot.command('new_application', async (ctx) => {
+    let application = [];
+
+    const chatId = ctx.message.from.id;
+    console.log(chatId)
+
+    function problem() {
+        ctx.replyWithHTML('<b>Что не работает</b>', Markup.keyboard(
+            [
+                [Markup.button.callback('Принтер')],
+                [Markup.button.callback('Компьютер')],
+                [Markup.button.callback('Мониор')]
+            ]
+        ))
+    }
+
+
+    let appl = [
+        'Диагностический центр',
+        '1 поликлиника',
+        '2 поликлиника',
+        '3 поликлиника',
+        '4 поликлиника',
+    ]
+
+    try {
+        await ctx.replyWithHTML('<b>Какое подразделение</b>', Markup.keyboard(
+            [
+                [Markup.button.callback(appl[0])],
+                [Markup.button.callback('1 поликлиника'), Markup.button.callback('2 поликлиника')],
+                [Markup.button.callback('3 поликлиника'), Markup.button.callback('4 поликлиника')],
+                [Markup.button.callback('10 поликлиника'), Markup.button.callback('Женская консультация')],
+                [Markup.button.callback('ТП'), Markup.button.callback('ЦМР')],
+            ]
+        ))
+
+for ( i of appl) {
+
+    bot.hears(i, async (ctx) => {
+        console.log(ctx.message.text)
+        application.push(ctx.message.text)
+        problem()
+    })
+} 
+
+
+        // bot.hears('Диагностический центр', async (ctx) => {
+        //     console.log(ctx.message.text)
+        //     application.push(ctx.message.text)
+        //     problem()
+        // })
+
+
+        // bot.hears('ТП', async (ctx) => {
+        //     console.log(ctx.message.text)
+        //     application.push(ctx.message.text)
+        //     problem()
+        // })
+
+
+        // bot.hears('ЦМР', async (ctx) => {
+        //     console.log(ctx.message.text)
+        //     application.push(ctx.message.text)
+        //     problem()
+        // })
+
+
+
+
+
+
+        bot.hears('Принтер', async (ctx) => {
+            console.log(ctx.message.text)
+            application.push(ctx.message.text)
+            for (i of application) {
+                botMessage.sendMessage('347867666', i);
+                botMessage.sendMessage(chatId, i);
+
+            }
+            await ctx.replyWithHTML('Какой кабинет', Markup.keyboard(
+                [
+                    [Markup.button.callback('1')],
+                    [Markup.button.callback('2')],
+                    [Markup.button.callback('3')]
+                ]
+            ))
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        console.log(application)
+    } catch (e) {
+        console.error(e)
+    }
+})
+
+
+// bot.on('text', (ctx) => ctx.reply(`Привет ${ctx.message.from.first_name} ${text.command}`))
 bot.launch()
 
-bot.on('message', async (ctx) => {
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
-    const keyboard = Keyboard.make([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], {
-        pattern: [3, 1, 1]
-    })
-    
-    console.log(keyboard)
-  
-    await ctx.reply('Simple built-in keyboard', keyboard.reply())
 
-    // await ctx.reply('Simple inline keyboard', keyboard.inline())
-    console.log(keyboard.buttons)
-  })
+
 
 
 
